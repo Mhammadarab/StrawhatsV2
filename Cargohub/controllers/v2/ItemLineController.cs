1bf07ad8484af9127b9c76cfa508415e9c4f9473
Beratoshh <berat_kartal05@hotmail.com>
2024-10-20 22:18:40 +0200

    Update ItemLineController.cs

--- Cargohub/controllers/v2/ItemLineController.cs
+++ Cargohub/controllers/v2/ItemLineController.cs
@@ -12,76 +12,10 @@ namespace Cargohub.Controllers.v2
     [ApiController]
     public class ItemLineController : Controller
     {
-        private readonly ICrudService<ItemLine, int> _itemLineService;
-
-        public ItemLineController(ICrudService<ItemLine, int> itemLineService)
-        {
-            _itemLineService = itemLineService;
-        }
-
         [HttpGet]
         public IActionResult GetItemLines()
         {
-            var itemLines = _itemLineService.GetAll();
-            return Ok(itemLines);
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetItemLineById(int id)
-        {
-            var itemLine = _itemLineService.GetById(id);
-            if (itemLine == null)
-            {
-                return NotFound();
-            }
-            return Ok(itemLine);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateItemLine([FromBody] ItemLine itemLine)
-        {
-            if (itemLine == null)
-            {
-                return BadRequest();
-            }
-
-            await _itemLineService.Create(itemLine);
-            return CreatedAtAction(nameof(GetItemLineById), new { id = itemLine.Id }, itemLine);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateItemLine(int id, [FromBody] ItemLine itemLine)
-        {
-            if (itemLine == null || id != itemLine.Id)
-            {
-                return BadRequest();
-            }
-
-            try
-            {
-                await _itemLineService.Update(itemLine);
-            }
-            catch (KeyNotFoundException)
-            {
-                return NotFound();
-            }
-
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteItemLine(int id)
-        {
-            try
-            {
-                await _itemLineService.Delete(id);
-            }
-            catch (KeyNotFoundException)
-            {
-                return NotFound();
-            }
-
-            return NoContent();
+            return Ok("List of all Item Lines in v2");
         }
     }
 }
