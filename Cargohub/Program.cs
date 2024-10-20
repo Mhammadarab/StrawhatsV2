4ba8be543484390820c27b21d104f2015b5e2d44
Mhammadarab <mohammadarab044@gmail.com>
2024-10-21 00:26:02 +0200

    Made Suppliers CRUD

--- Cargohub/Program.cs
+++ Cargohub/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddSingleton<ICrudService<Warehouse, int>, WarehouseService>();
 builder.Services.AddSingleton<ICrudService<ItemLine, int>, ItemLineService>();
 builder.Services.AddSingleton<ICrudService<Transfer, int>, TransferService>();
 builder.Services.AddSingleton<ICrudService<Order, int>, OrderService>();
+builder.Services.AddSingleton<ICrudService<Supplier, int>, SupplierService>();
+
 
 builder.Services.AddSingleton<ICrudService<ItemGroup, int>, ItemGroupService>();
 
