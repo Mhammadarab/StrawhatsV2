f09e8d3361726373d15e8d5b6ab5c2a289998db2
Mhammadarab <mohammadarab044@gmail.com>
2025-01-13 21:20:19 +0100

    reload users on start

--- Program.cs
+++ Program.cs
@@ -107,6 +107,7 @@ if (app.Environment.IsDevelopment())
 }
 
 
+AuthProvider.ReloadUsers();
 app.UseAuthorization();
 app.Use(async (ctx, next) =>
 {
@@ -125,7 +126,6 @@ app.Use(async (ctx, next) =>
         await ctx.Response.WriteAsync("API key is missing");
         return;
     }
-
     var user = AuthProvider.GetUser(apiKey);
     if (user == null)
     {
